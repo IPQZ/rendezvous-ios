bdfd9f58b74113d55dfb582eca628411d240c247
zsniper <zkman88@gmail.com>
2015-01-31 23:01:51 -0500

    changed hourlytableviewcontroller to normal view controller

---
+++ Rendezvous/Rendezvous/HourlyViewController.h
@@ -0,0 +1,15 @@
+//
+//  HourlyViewController.h
+//  Rendezvous
+//
+//  Created by zkmb on 2015-01-31.
+//  Copyright (c) 2015 IPQZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HourlyViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSMutableArray *hobbyData;
+
+@end
